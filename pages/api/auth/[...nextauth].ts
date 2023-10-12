20256b69001eb60782a95e2c41627a893e2b3952
Ibrahim Kalam <95757707+Ibra-Zeno@users.noreply.github.com>
2023-10-12 19:12:16 +0100

    Removed comments

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,33 +1,3 @@
-/* import { NextApiHandler } from "next";
-import NextAuth, { AuthOptions } from "next-auth";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import GithubProvider from "next-auth/providers/github";
-// import EmailProvider from "next-auth/providers/email";
-import prisma from "../../../lib/prisma";
-
-const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options);
-
-const options: AuthOptions = {
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID || "",
-      clientSecret: process.env.GITHUB_SECRET || "",
-    }),
-    // EmailProvider({
-    //   server: {
-    //     host: process.env.EMAIL_SERVER_HOST,
-    //     port: process.env.EMAIL_SERVER_PORT,
-    //     auth: {
-    //       user: process.env.EMAIL_SERVER_USER,
-    //       pass: process.env.EMAIL_SERVER_PASSWORD,
-    //     },
-    //   },
-    //   from: process.env.EMAIL_FROM,
-    // }),
-  ],
-  adapter: PrismaAdapter(prisma),
-  secret: process.env.NEXTAUTH_SECRET,
-}; */
 import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
 import NextAuth, { SessionStrategy } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
@@ -78,35 +48,3 @@ const authHandler: NextApiHandler = async (
 };
 
 export default authHandler;
-
-/* CredentialsProvider({
-      name: "Demo Account",
-      credentials: {
-        username: { label: "Username", type: "text", placeholder: "demo" },
-        password: {
-          label: "Password",
-          type: "password",
-          placeholder: "demopassword",
-        },
-      },
-      async authorize(
-        credentials: Record<"username" | "password", string> | undefined,
-        req: Pick<NextApiRequest, "body" | "headers" | "method">,
-      ): Promise<User | null> {
-        // Check if the provided credentials match your demo account
-        if (
-          credentials?.username === "demo" &&
-          credentials?.password === "demopassword"
-        ) {
-          // Return a user object for the demo account
-          return Promise.resolve({
-            id: 1,
-            name: "Demo User",
-            email: "demo@example.com",
-          });
-        }
-
-        // Return null if the credentials are invalid
-        return Promise.resolve(null);
-      },
-    }), */
