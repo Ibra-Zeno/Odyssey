29203bac19b2be0a009af149967b4109f4502fa7
Ibrahim Kalam <95757707+Ibra-Zeno@users.noreply.github.com>
2023-10-13 12:28:38 +0100

    New bg on layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from "react";
 import Header from "./Header";
 import Hero from "./Hero";
+import Image from "next/image";
 import { useRouter } from "next/router";
 
 interface LayoutProps {
@@ -12,9 +13,20 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
   const isActive = (pathname: string) => router.pathname === pathname;
   return (
     <div className="relative flex h-full w-screen flex-col items-center justify-center bg-palBg">
-      <Header />
-      {isActive("/") && <Hero />}
-      <div className="container mx-auto p-8">{children}</div>
+      <div className="absolute inset-0 z-0">
+        <Image
+          src="../images/bg-layout.svg"
+          alt="Hero Background"
+          layout="fill"
+          className="opacity-20"
+          objectFit="cover"
+        ></Image>
+      </div>
+      <div className="z-10 w-full">
+        <Header />
+        {isActive("/") && <Hero />}
+        <div className="container mx-auto p-8">{children}</div>
+      </div>
     </div>
   );
 };
