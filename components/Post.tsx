ef18eb838ec0fed62d09e278cf33e5e31bc49222
Ibrahim Kalam <95757707+Ibra-Zeno@users.noreply.github.com>
2023-10-13 12:29:37 +0100

    changed tag location

--- components/Post.tsx
+++ components/Post.tsx
@@ -16,7 +16,26 @@ const Post: React.FC<{ post: PostProps }> = ({ post }) => {
   };
 
   return (
-    <div className="rounded p-8">
+    <div className="rounded p-8 pt-2">
+      <div className="mb-2 mt-4 flex items-center">
+        <div className="flex items-center space-x-2">
+          {post.tags.length > 0 && (
+            <div className="flex items-center space-x-2">
+              {post.tags.map((postTag) => (
+                <Badge
+                  variant="outline"
+                  key={postTag.tag.id}
+                  className={`font-noto text-[11px] tracking-wider text-gray-800 shadow-md ${
+                    tagColourMap[postTag.tag.name] || "bg-gray-300"
+                  } border-gray-300/30`}
+                >
+                  {postTag.tag.name}
+                </Badge>
+              ))}
+            </div>
+          )}
+        </div>
+      </div>
       <h2
         className="font-display text-xl font-semibold"
         onClick={handlePostClick}
@@ -39,33 +58,14 @@ const Post: React.FC<{ post: PostProps }> = ({ post }) => {
           <p className="font-noto text-xs">{authorName}</p>
         </Link>
       </div>
-      <div className="mt-4 flex items-center">
-        <div className="flex items-center space-x-2">
-          {post.tags.length > 0 && (
-            <div className="flex items-center space-x-2">
-              {post.tags.map((postTag) => (
-                <Badge
-                  variant="outline"
-                  key={postTag.tag.id}
-                  className={`font-display tracking-wide text-gray-800 shadow-md ${
-                    tagColourMap[postTag.tag.name] || "bg-gray-300"
-                  } border-gray-300/30`}
-                >
-                  {postTag.tag.name}
-                </Badge>
-              ))}
-            </div>
-          )}
-        </div>
-      </div>
       <div
-        className="max-h-16 overflow-y-hidden font-noto text-sm"
+        className=" mt-4 max-h-16 overflow-y-hidden font-noto text-sm"
         dangerouslySetInnerHTML={{ __html: post.content || "" }}
         onClick={handlePostClick}
       />
       {post.published && (
         <>
-          <div className="mt-4 flex items-end">
+          <div className="mt-4 flex items-end gap-x-4">
             <Like post={post} />
             <div className="flex flex-row items-end text-sm">
               <MessageCircle size={16} className="fill-none" />
