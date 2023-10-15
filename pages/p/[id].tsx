d42b0e89aa67d1e41b71606163a340fbe3f6fd6a
Ibrahim Kalam <95757707+Ibra-Zeno@users.noreply.github.com>
2023-10-16 00:18:51 +0100

    Added layout and formatting to post pages

--- pages/p/[id].tsx
+++ pages/p/[id].tsx
@@ -1,9 +1,11 @@
 import prisma from "../../lib/prisma";
 import { useSession } from "next-auth/react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { useRouter } from "next/router";
 import { GetServerSideProps } from "next";
 import Layout from "../../components/Layout";
 import Link from "next/link";
+import { MessageCircle } from "lucide-react";
 import { PostProps } from "../../utils/types";
 import Comment from "../../components/Comments";
 import Like from "../../components/Like";
@@ -24,9 +26,7 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
       include: {
         Like: { select: { id: true } },
         Comment: { select: { id: true } },
-        author: {
-          select: { name: true, email: true },
-        },
+        author: true,
         tags: {
           include: { tag: true },
         },
@@ -63,6 +63,8 @@ const Post: React.FC<PostProps> = (props) => {
   const userHasValidSession = Boolean(session);
   const postBelongsToUser = session?.user?.email === props.author?.email;
   const title = !props.published ? `${props.title} (Draft)` : props.title;
+  const authorName = props.author ? props.author.name : "Unknown author";
+  const avatarImage = props?.author?.image || undefined;
 
   const publishPost = async (id: string) => {
     await fetch(`/api/publish/${id}`, {
@@ -84,29 +86,51 @@ const Post: React.FC<PostProps> = (props) => {
 
   return (
     <Layout>
-      <div className="page">
+      <div className="page mx-auto max-w-3xl">
         <h2 className="mb-4 text-2xl font-bold">{title}</h2>
-        <Link href={`/profile/${props.author?.email}`}>
-          <p className="mb-4 text-sm hover:text-red-500">
-            By {props?.author?.name || "Unknown author"}
-          </p>
-        </Link>
+        <div className="mt-4 flex flex-row gap-x-2">
+          <Link
+            className="flex flex-row gap-x-2"
+            href={
+              authorName !== "Unknown author"
+                ? `/profile/${props.author?.email}`
+                : ""
+            }
+          >
+            <Avatar className="h-8 w-8">
+              <AvatarImage src={avatarImage} alt={authorName ?? undefined} />
+              <AvatarFallback className="">{authorName}</AvatarFallback>
+            </Avatar>
+            <p className="font-noto text-xs italic text-pal3">{authorName}</p>
+          </Link>
+        </div>
+        {props.published && (
+          <div className="mb-4 flex flex-row items-center gap-x-4 border-y border-y-pal3/40 py-3">
+            <Like post={props} />
+            <div className="flex flex-row items-end text-sm">
+              <MessageCircle size={16} className="fill-none text-pal3" />
+              <span className="ml-1 text-pal3">{props.Comment.length}</span>
+            </div>
+          </div>
+        )}
         {props.tags && (
-          <div className="flex items-center space-x-2">
-            <span className="font-semibold">Tags:</span>
+          <div className="mb-6 flex items-center space-x-2">
             {props.tags.map((postTag) => (
               <span
                 key={postTag.tag.id}
-                className={`text-gray-800 shadow-md ${
+                className={`font-display font-semibold tracking-wider text-gray-800 shadow-md ${
                   tagColourMap[postTag.tag.name] || "bg-gray-300"
-                } rounded-md px-2 py-1 text-sm`}
+                } rounded-md px-2 py-1 text-xs`}
               >
                 {postTag.tag.name}
               </span>
             ))}
           </div>
         )}
-        <div dangerouslySetInnerHTML={{ __html: props.content ?? "" }} />
+        <div
+          className="font-noto text-[20px] leading-[32px]"
+          dangerouslySetInnerHTML={{ __html: props.content ?? "" }}
+        />
         {!props.published && userHasValidSession && postBelongsToUser && (
           <button
             onClick={() => publishPost(props.id)}
@@ -133,7 +157,6 @@ const Post: React.FC<PostProps> = (props) => {
         )}
         {props.published && (
           <>
-            <Like post={props} />
             <Comment postId={props.id} />
           </>
         )}
