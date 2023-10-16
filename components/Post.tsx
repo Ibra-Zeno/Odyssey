4d2081321cbd0ead202f3aa7d18a6a9e9d5c1d36
Ibrahim Kalam <95757707+Ibra-Zeno@users.noreply.github.com>
2023-10-16 21:42:09 +0100

    Stylistic update

--- components/Post.tsx
+++ components/Post.tsx
@@ -18,45 +18,46 @@ const Post: React.FC<{ post: PostProps }> = ({ post }) => {
   return (
     <div className="rounded px-8 py-8">
       <div className="mb-2 mt-4 flex items-center">
-        <div className="flex items-center space-x-2">
-          {post.tags.length > 0 && (
-            <div className="-mt-4 flex items-center space-x-2 pb-4">
-              {post.tags.map((postTag) => (
-                <Badge
-                  variant="outline"
-                  key={postTag.tag.id}
-                  className={`font-display text-[11px] font-semibold tracking-wider text-gray-800 shadow-md ${
-                    tagColourMap[postTag.tag.name] || "bg-gray-300"
-                  } border-gray-300/30`}
-                >
-                  {postTag.tag.name}
-                </Badge>
-              ))}
-            </div>
-          )}
-        </div>
+        <div className="flex items-center space-x-2"></div>
       </div>
       <h2
-        className="font-display text-xl font-semibold text-pal3"
+        className="mb-4 font-display text-xl font-semibold text-pal3"
         onClick={handlePostClick}
       >
         {post.title}
       </h2>
-      <div className="mt-4 flex flex-row gap-x-2">
-        <Link
-          className="flex flex-row gap-x-2"
-          href={
-            authorName !== "Unknown author"
-              ? `/profile/${post.author?.email}`
-              : ""
-          }
-        >
-          <Avatar className="h-5 w-5">
-            <AvatarImage src={avatarImage} alt={authorName ?? undefined} />
-            <AvatarFallback className="">{authorName}</AvatarFallback>
-          </Avatar>
-          <p className="font-noto text-xs italic text-pal3">{authorName}</p>
-        </Link>
+      <div className="mb-3 flex flex-row justify-between">
+        {post.tags.length > 0 && (
+          <div className="flex items-center space-x-2">
+            {post.tags.map((postTag) => (
+              <Badge
+                variant="outline"
+                key={postTag.tag.id}
+                className={`font-display text-[11px] font-semibold tracking-wider text-gray-800 shadow-md ${
+                  tagColourMap[postTag.tag.name] || "bg-gray-300"
+                } border-gray-300/30`}
+              >
+                {postTag.tag.name}
+              </Badge>
+            ))}
+          </div>
+        )}
+        <div className="flex flex-row gap-x-2">
+          <Link
+            className="flex flex-row gap-x-2"
+            href={
+              authorName !== "Unknown author"
+                ? `/profile/${post.author?.email}`
+                : ""
+            }
+          >
+            <Avatar className="h-5 w-5">
+              <AvatarImage src={avatarImage} alt={authorName ?? undefined} />
+              <AvatarFallback className="">{authorName}</AvatarFallback>
+            </Avatar>
+            <p className="font-noto text-xs italic text-pal3">{authorName}</p>
+          </Link>
+        </div>
       </div>
       <div
         className=" mt-4 max-h-16 overflow-y-hidden font-noto text-sm text-pal3"
