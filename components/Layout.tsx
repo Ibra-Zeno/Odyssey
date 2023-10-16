fef291c67a529b3fa6c3a004108151cc0c55aa2f
Ibrahim Kalam <95757707+Ibra-Zeno@users.noreply.github.com>
2023-10-16 21:41:57 +0100

    Layout change

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -7,10 +7,10 @@ interface LayoutProps {
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <div className="background-bamboo relative flex h-full w-screen flex-col items-center justify-center ">
-      <div className="z-10 w-full">
+    <div className="background-bamboo relative flex h-full min-h-screen w-screen flex-col items-center justify-center ">
+      <div className="relative z-10 h-full w-full">
         <Header />
-        <div className=" container mx-auto p-8">{children}</div>
+        <div className="container mx-auto min-h-screen p-8">{children}</div>
       </div>
     </div>
   );
