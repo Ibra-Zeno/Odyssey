62faf6fe2b0673ffb248ac6bfbf3a338db15b5f3
Ibrahim Kalam <95757707+Ibra-Zeno@users.noreply.github.com>
2023-10-18 14:36:55 +0100

    Changed bg

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -7,7 +7,7 @@ interface LayoutProps {
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <div className="relative flex h-full min-h-screen w-screen flex-col items-center justify-center bg-gradient-to-b from-gray-900 to-gray-700 ">
+    <div className="relative flex h-full min-h-screen w-screen flex-col items-center justify-center bg-gradient-to-b from-slate-900 to-slate-700 ">
       <div className="relative z-10 h-full w-full">
         <Header />
         <div className="container mx-auto min-h-screen p-8">{children}</div>
