f6bad2c694119384c777f53f1085ecae093e0c8c
Ibrahim Kalam <95757707+Ibra-Zeno@users.noreply.github.com>
2023-10-18 04:04:37 +0100

    Change but needs more change

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,9 +4,9 @@ import { Button } from "./ui/button";
 
 const Hero: React.FC = () => {
   return (
-    <div className="relative h-full w-full px-8 ">
-      <div className="bg-moon relative mx-auto mt-4 flex aspect-[16/8] max-w-7xl justify-center rounded-lg bg-stone-400/40  pt-16 ">
-        <div className="mx-auto max-w-3xl text-center">
+    <div className="absolute inset-0 h-full min-h-[60rem] w-full px-8 ">
+      <div className="bg-moon  mx-auto mt-4 flex w-full justify-center rounded-lg pt-16 ">
+        <div className="mx-auto flex max-w-3xl flex-col justify-center text-center">
           <h1 className="block font-display text-3xl font-bold text-pal3 sm:text-4xl md:text-5xl">
             Leave Your Mark on the World
           </h1>
@@ -27,13 +27,13 @@ const Hero: React.FC = () => {
         </div>
 
         <div className="absolute -bottom-32 z-30 mx-auto mt-20 flex h-auto max-w-4xl justify-center">
-          <Image
+          {/* <Image
             src={map}
             alt="hero"
             width={896}
             height={384}
             className="mx-auto flex min-h-[24rem] w-auto justify-center rounded-xl object-cover object-center"
-          ></Image>
+          ></Image> */}
         </div>
       </div>
     </div>
