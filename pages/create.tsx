d312a94d41822eebfd9ac383c49cd5c10dd0105e
Ibrahim Kalam <95757707+Ibra-Zeno@users.noreply.github.com>
2023-10-19 16:09:55 +0100

    Toast and Styling

--- pages/create.tsx
+++ pages/create.tsx
@@ -1,9 +1,13 @@
 import { useState } from "react";
 import { useRouter } from "next/router";
+import Image from "next/image";
 import dynamic from "next/dynamic";
+import { useToast } from "@/components/ui/use-toast";
 import Layout from "../components/Layout";
 import { tagsArray } from "../utils/tags";
 import Select from "react-select";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 // Dynamically import the QuillEditor component
 const QuillEditor = dynamic(() => import("../components/QuillEditor"), {
@@ -16,6 +20,7 @@ const Draft: React.FC = () => {
   const [content, setContent] = useState("");
   const [selectedTags, setSelectedTags] = useState<string[]>([]);
   const router = useRouter();
+  const { toast } = useToast();
 
   // Send a POST request to the API to create a new draft
   const submitData = async (e: React.SyntheticEvent) => {
@@ -48,10 +53,19 @@ const Draft: React.FC = () => {
 
   return (
     <Layout>
-      <div className="flex items-center justify-center bg-gray-100 p-12">
-        <form onSubmit={submitData} className="flex flex-col">
-          <h1 className="mb-4 text-xl">New Draft</h1>
-          <input
+      <div className="pointer-events-none absolute inset-0 -z-0">
+        <Image
+          src="/images/Moon.svg"
+          alt="Hero"
+          layout="fill"
+          objectFit="cover"
+          objectPosition="center"
+        ></Image>
+      </div>
+      <div className="isolate mx-auto flex min-h-[80vh] max-w-6xl items-center justify-center rounded bg-[#ffe5ca]/75 p-12 shadow-lg">
+        <form onSubmit={submitData} className="flex w-full max-w-3xl flex-col">
+          <h1 className="mb-4 font-display text-2xl font-bold">New Draft</h1>
+          <Input
             autoFocus
             onChange={(e) => setTitle(e.target.value)}
             placeholder="Title"
@@ -59,31 +73,41 @@ const Draft: React.FC = () => {
             value={title}
             className="mb-4 rounded-md border p-2"
           />
-          <label htmlFor="tags" className="mb-1 block">
-            Select Tags
-          </label>
           <Select
             options={options}
             isMulti
+            placeholder="Select tags"
+            className="rounded-md font-noto text-sm selection:ring selection:ring-pal2 focus:ring focus:ring-pal2"
             onChange={(selected) =>
               setSelectedTags(selected.map((tag) => tag.value))
             }
             value={selectedTags.map((tag) => ({ value: tag, label: tag }))}
           />
-          <QuillEditor content={content} setContent={setContent} />
-          <button
-            type="submit"
-            disabled={!content || !title}
-            className="mb-4 rounded-md bg-gray-300 px-6 py-2"
-          >
-            Create
-          </button>
-          <a
-            className="ml-4 cursor-pointer text-blue-500 hover:underline"
-            onClick={() => router.push("/")}
-          >
-            or Cancel
-          </a>
+          <div className="mt-6 min-h-[300px] rounded-lg bg-white font-noto">
+            <QuillEditor content={content} setContent={setContent} />
+          </div>
+          <div className="mx-auto mt-6 flex flex-row gap-x-6">
+            <Button
+              type="submit"
+              disabled={!content && !title}
+              onClick={() => {
+                toast({
+                  title: "Post created 🎉",
+                  description: "Your post has been saved to drafts.",
+                });
+              }}
+              className="mb-4 rounded-md px-6 py-2"
+            >
+              Create
+            </Button>
+            <Button
+              className="ml-4 cursor-pointer"
+              variant={"destructive"}
+              onClick={() => router.push("/")}
+            >
+              Cancel
+            </Button>
+          </div>
         </form>
       </div>
     </Layout>
