ecfb2ad5e25eeb614b929c99653ab25c9fd4d887
Ibrahim Kalam <95757707+Ibra-Zeno@users.noreply.github.com>
2023-10-19 21:06:54 +0100

    Log in buttons

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,8 +2,11 @@ import Image from "next/image";
 import map from "../public/images/wasHereBg.svg";
 import { Button } from "./ui/button";
 import Link from "next/link";
+import { useSession } from "next-auth/react";
 
 const Hero: React.FC = () => {
+  const { data: session, status } = useSession();
+
   return (
     <div className="h-full min-h-[34rem] w-full px-8 ">
       <div className="absolute inset-0 -z-10 w-full">
@@ -24,18 +27,40 @@ const Hero: React.FC = () => {
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum
             velit possimus temporibus ut totam
           </p>
-          <div className="mt-8 sm:mt-8 sm:flex sm:justify-center lg:justify-center">
-            <div className="rounded-md shadow">
-              <Link href="/api/auth/signin">
+          {!session ? (
+            <div className="mt-8 sm:mt-8 sm:flex sm:justify-center lg:justify-center">
+              <div className="rounded-md shadow">
+                <Link href="/api/auth/signin">
+                  <Button
+                    size="lg"
+                    className="bg-pal2 text-base font-bold text-pal3 shadow-lg hover:bg-pal6"
+                  >
+                    Get started
+                  </Button>
+                </Link>
+              </div>
+            </div>
+          ) : (
+            <div className="mt-8 flex justify-center gap-x-6 sm:mt-8 sm:flex">
+              <Link href="/create">
                 <Button
                   size="lg"
-                  className="bg-pal2 text-base font-bold text-pal3 shadow-lg hover:bg-pal4"
+                  className="bg-pal2 text-base font-bold text-pal3 shadow-lg hover:bg-pal6"
                 >
-                  Get started
+                  New Post
+                </Button>
+              </Link>
+              <Link href="/drafts">
+                <Button
+                  size="lg"
+                  variant={"outline"}
+                  className="bg-transparent text-base font-bold text-pal3 shadow-lg backdrop-blur-sm hover:border-pal5 hover:bg-pal5 hover:text-pal3"
+                >
+                  View Drafts
                 </Button>
               </Link>
             </div>
-          </div>
+          )}
         </div>
       </div>
     </div>
