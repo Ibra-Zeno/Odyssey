c3eada2b361f1f8fa70f92ace31681fddca9b184
Ibrahim Kalam <95757707+Ibra-Zeno@users.noreply.github.com>
2023-10-21 16:15:06 +0100

    initial design

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,15 @@
 import { Orbit } from "lucide-react";
+import Link from "next/link";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="flex w-full flex-col items-center justify-center gap-y-3 border-t border-gray-200/30 py-5">
-      <Orbit className="animate-spin-slow text-stone-50" />
-      <p className="text-gray-200">Made by Ibrahim Kalam </p>
+    <footer className="mt-10 flex w-full flex-col items-center justify-center gap-y-6 border-t-2 border-t-gray-200/30 py-5 ">
+      <Link href="/">
+        <Orbit className="h-9 w-9 animate-spin-slow cursor-pointer text-[#f0ddeb] transition-colors hover:text-[#bea5ab]" />
+      </Link>
+      <p className="font-noto text-sm font-extralight italic text-[#f0ddeb]">
+        Made by Ibrahim Kalam{" "}
+      </p>
     </footer>
   );
 };
