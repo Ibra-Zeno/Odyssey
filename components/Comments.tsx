26491885472ee6d5d01a53f645741a8b69f99593
Ibrahim Kalam <95757707+Ibra-Zeno@users.noreply.github.com>
2023-10-21 21:34:05 +0100

    Redesign

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -1,5 +1,10 @@
 import { useState, useEffect } from "react";
 import { useSession } from "next-auth/react";
+import Link from "next/link";
+import { XCircle } from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import { Textarea } from "./ui/textarea";
 
 const Comment: React.FC<{ postId: string }> = ({ postId }) => {
   const { data: session } = useSession();
@@ -49,45 +54,86 @@ const Comment: React.FC<{ postId: string }> = ({ postId }) => {
   };
 
   return (
-    <div className="my-4">
-      <h3 className="mb-2 text-lg font-semibold">Comments</h3>
+    <div className="my-4 border-t-2 border-t-stone-600/10 pt-8">
       {session ? (
         <div className="mb-4">
-          <textarea
+          <Textarea
             value={content}
             onChange={(e) => setContent(e.target.value)}
             placeholder="Add a comment..."
-            className="w-full rounded border p-2"
+            className="w-full rounded bg-stone-100 p-2 shadow-sm"
           />
-          <button
+          <Button
             onClick={submitComment}
-            className="mt-2 rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600"
+            className="mt-2.5 h-fit rounded bg-pal5 px-4 py-2.5 font-display text-xs font-medium tracking-wide text-stone-100 shadow-md hover:bg-pal5/80"
           >
             Submit
-          </button>
+          </Button>
         </div>
       ) : (
-        <p>Please sign in to leave a comment.</p>
+        <p className="font-display text-sm font-medium text-stone-700">
+          Please sign in to leave a comment.
+        </p>
       )}
-      <div className="mt-4">
-        {comments.length > 0 ? (
-          comments.map((comment: any) => (
-            <div key={comment.id} className="relative mb-2 border p-2">
-              <p>{comment.content}</p>
-              {comment.author && <small>By {comment.author.name}</small>}
-              {session?.user?.email === comment.author?.email && (
-                <button
-                  onClick={() => handleDeleteClick(comment.id)}
-                  className="absolute right-10 top-6 ml-4 cursor-pointer text-red-500 hover:underline"
-                >
-                  Delete
-                </button>
-              )}
-            </div>
-          ))
-        ) : (
-          <p>No comments yet.</p>
-        )}
+      <div className="mt-8">
+        <h3 className="mb-3  pl-0.5 font-display text-base font-bold text-stone-700">
+          Comments
+        </h3>
+        <section className="flex flex-col gap-y-1">
+          {comments.length > 0 ? (
+            comments.map((comment: any) => (
+              <div
+                key={comment.id}
+                className="relative mb-2 flex flex-row items-start justify-between gap-x-4 rounded border-b-2 border-stone-600/5 p-2 shadow-sm"
+              >
+                <div className="flex flex-col">
+                  <p className=" text-justify font-noto text-sm text-stone-700">
+                    {comment.content}
+                  </p>
+                  {comment.author && (
+                    <Link
+                      className="mt-2 flex cursor-pointer flex-row items-center gap-x-2"
+                      href={
+                        comment.author.name !== "Unknown author"
+                          ? `/profile/${comment.author?.email}`
+                          : ""
+                      }
+                    >
+                      <Avatar className="h-5 w-5">
+                        <AvatarImage
+                          src={comment.author.image}
+                          alt={comment.author.name ?? undefined}
+                        />
+                        <AvatarFallback className="">
+                          {comment.author.name}
+                        </AvatarFallback>
+                      </Avatar>
+                      <p className="font-noto text-xs italic text-stone-600 transition-colors duration-200 ease-in-out hover:text-stone-800">
+                        {comment.author.name}
+                      </p>
+                    </Link>
+                  )}
+                </div>
+                {session?.user?.email === comment.author?.email && (
+                  <Button
+                    onClick={() => handleDeleteClick(comment.id)}
+                    className="mr-2 h-fit cursor-pointer bg-transparent p-0 font-display text-xs font-medium tracking-wide text-stone-100 hover:bg-transparent "
+                  >
+                    <XCircle
+                      strokeWidth={1.3}
+                      size={24}
+                      className="fill-red-400 transition-colors duration-150 ease-in-out hover:fill-red-600"
+                    />
+                  </Button>
+                )}
+              </div>
+            ))
+          ) : (
+            <p className="font-display text-sm font-medium text-stone-700">
+              Be the first to comment!
+            </p>
+          )}
+        </section>
       </div>
     </div>
   );
