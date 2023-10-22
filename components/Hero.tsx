d7659232a9df679d5583c0e30b7d77ffa9e33de7
Ibrahim Kalam <95757707+Ibra-Zeno@users.noreply.github.com>
2023-10-22 14:58:00 +0100

    Title and subtitle

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -21,11 +21,11 @@ const Hero: React.FC = () => {
       <div className="relative mx-auto mt-4 flex justify-center rounded-lg pt-16 ">
         <div className="mx-auto flex max-w-3xl flex-col justify-center text-center">
           <h1 className="block font-display text-3xl font-bold text-pal3 sm:text-4xl md:text-5xl">
-            Leave Your Mark on the World
+            Oracles of the Odyssey
           </h1>
-          <p className="mt-6 text-lg text-gray-400">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum
-            velit possimus temporibus ut totam
+          <p className="mt-6 text-lg font-semibold text-gray-400">
+            Whispers of the Earth: A Global Vault Teeming with a Rich Tapestry
+            of Experiences, Emotions, and Imagination
           </p>
           {!session ? (
             <div className="mt-8 sm:mt-8 sm:flex sm:justify-center lg:justify-center">
