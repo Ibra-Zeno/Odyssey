508d9303575de690b352454414261adfdc2a7162
Ibrahim Kalam <95757707+Ibra-Zeno@users.noreply.github.com>
2023-10-23 19:09:11 +0100

    no overflow

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -9,7 +9,7 @@ interface LayoutProps {
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <div className="relative flex h-full min-h-screen w-screen flex-col items-center justify-center overflow-x-hidden bg-gradient-to-br from-[#450920] via-[#723346] to-[#a53860] ">
+    <div className="relative flex h-full min-h-screen w-screen flex-col items-center justify-center bg-gradient-to-br from-[#450920] via-[#723346] to-[#a53860] ">
       {/* dark (MAIN) red from-[#450920] to-[#a53860] */}
       {/* muted red from-[#564148]   to-[#4f4350] */}
       {/*orange from-[#8c2f39] to-[#b23a48] */}
@@ -17,7 +17,9 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
       {/* Grey from-[#2f4550] to-[#586f7c] */}
       <div className="relative z-10 h-full w-full">
         <Header />
-        <div className="container mx-auto min-h-screen p-8">{children}</div>
+        <div className="container mx-auto min-h-screen p-4 md:p-8">
+          {children}
+        </div>
         <Toaster />
         <Footer />
       </div>
