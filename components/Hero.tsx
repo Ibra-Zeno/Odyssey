e57a1b5b02a39fa6865b0fcea17c59a08d28a30e
Ibrahim Kalam <95757707+Ibra-Zeno@users.noreply.github.com>
2023-10-23 19:08:53 +0100

    mobile

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,7 +8,8 @@ const Hero: React.FC = () => {
   const { data: session, status } = useSession();
 
   return (
-    <div className="h-full min-h-[34rem] w-full px-8 ">
+    <div className="h-full w-full px-4 md:px-8 ">
+      {/* min-h-[34rem] */}
       <div className="absolute inset-0 -z-10 w-full">
         <Image
           src={map}
@@ -25,32 +26,32 @@ const Hero: React.FC = () => {
           className="flex w-full max-w-[100vw] justify-center rounded-xl object-cover object-center md:hidden"
         ></Image>
       </div>
-      <div className="relative mx-auto mt-4 flex justify-center rounded-lg sm:pt-6 md:pt-16 ">
+      <div className="relative mx-auto flex justify-center rounded-lg pt-6 md:pt-16 ">
         <div className="mx-auto flex max-w-3xl flex-col justify-center text-center">
           <h1 className="block font-display text-3xl font-bold text-pal3 sm:text-4xl md:text-5xl">
             Oracles of the Odyssey
           </h1>
-          <p className="mt-6 text-lg font-semibold text-gray-400">
+          <p className="mt-6 text-base font-semibold text-gray-400 sm:text-lg">
             Whispers of the Earth: A Global Vault Teeming with a Rich Tapestry
             of Experiences, Emotions, and Imagination
           </p>
           {!session ? (
-            <div className="mt-8 sm:mt-8 sm:flex sm:justify-center lg:mt-9">
+            <div className="my-8 sm:mt-8 sm:flex sm:justify-center lg:my-10">
               <Link href="/api/auth/signin">
                 <Button
                   size="lg"
-                  className="w-fit bg-pal4 px-6 font-display text-base font-bold tracking-wide text-stone-50 shadow-lg shadow-palBg/25 hover:bg-pal6"
+                  className="w-fit bg-pal4 font-display text-sm font-bold tracking-wide text-stone-50 transition-all duration-300 ease-in-out hover:bg-pal6 md:text-base"
                 >
                   Get started
                 </Button>
               </Link>
             </div>
           ) : (
-            <div className="mt-8 flex flex-col justify-center gap-x-6 gap-y-4 sm:mt-8 sm:flex md:flex-row">
+            <div className="my-8 flex flex-col justify-center gap-x-6 gap-y-4 sm:flex md:flex-row lg:my-10">
               <Link href="/create">
                 <Button
                   size="lg"
-                  className="w-full bg-pal4 font-display text-base font-bold tracking-wide text-stone-50 shadow-lg hover:bg-pal6 md:w-fit md:px-6"
+                  className="w-full bg-pal4 font-display text-sm font-bold tracking-wide text-stone-50 transition-all duration-200 ease-in-out hover:bg-pal6 md:w-fit md:text-base"
                 >
                   New Post
                 </Button>
@@ -59,7 +60,7 @@ const Hero: React.FC = () => {
                 <Button
                   size="lg"
                   variant={"outline"}
-                  className="w-full bg-transparent font-display text-base font-bold tracking-wide text-pal3 shadow-lg backdrop-blur-sm hover:border-pal5 hover:bg-pal5 hover:text-pal3 md:w-fit"
+                  className="w-full bg-transparent font-display text-sm font-bold tracking-wide text-pal3 backdrop-blur-sm transition-all duration-200 ease-in-out hover:border-pal5 hover:bg-pal5 hover:text-pal3 focus:border-pal5 focus:bg-pal5 focus:text-pal3 md:w-fit md:text-base"
                 >
                   View Drafts
                 </Button>
