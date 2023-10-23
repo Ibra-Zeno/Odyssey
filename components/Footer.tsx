d6b4781c5e28471ac14235c0dcadd63f2984c40d
Ibrahim Kalam <95757707+Ibra-Zeno@users.noreply.github.com>
2023-10-23 19:08:35 +0100

    Sm mobile logo

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,10 +5,10 @@ const Footer: React.FC = () => {
   return (
     <footer className="mt-10 flex w-full flex-col items-center justify-center gap-y-6 border-t-2 border-t-gray-200/30 py-5 ">
       <Link href="/">
-        <Orbit className="h-9 w-9 animate-spin-slow cursor-pointer text-[#f0ddeb] transition-colors hover:text-[#bea5ab]" />
+        <Orbit className="h-6 w-6 animate-spin-slow cursor-pointer text-[#f0ddeb] transition-colors hover:text-[#bea5ab] sm:h-9 sm:w-9" />
       </Link>
-      <p className="font-noto text-sm font-extralight italic text-[#f0ddeb]">
-        Made by Ibrahim Kalam{" "}
+      <p className="font-noto text-xs font-extralight italic text-[#f0ddeb] md:text-sm">
+        Made by Ibrahim Kalam
       </p>
     </footer>
   );
